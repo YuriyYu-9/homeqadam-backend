78de7bbc73a91b03b6bac9e2dbff3db208b6b0f9
Yuri <ugajurij123@gmail.com>
2025-12-11 17:23:51 +0500

    Initial project setup

---
+++ src/main/java/com/homeqadam/backend/HomeqadamBackendApplication.java
@@ -0,0 +1,13 @@
+package com.homeqadam.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeqadamBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeqadamBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/homeqadam/backend/HomeqadamBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homeqadam.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeqadamBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
